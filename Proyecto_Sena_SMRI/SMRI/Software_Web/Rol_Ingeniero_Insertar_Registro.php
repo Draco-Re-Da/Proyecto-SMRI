974ff1cfb69592cb3a32c84aca65d901faa6d4c7
DracoReDa <renanduque3@gmail.com>
2024-02-06 10:48:44 -0500

    Creacion insertar registro del rol ingeniero

--- Proyecto_Sena_SMRI/SMRI/Software_Web/Rol_Ingeniero_Insertar_Registro.php
+++ Proyecto_Sena_SMRI/SMRI/Software_Web/Rol_Ingeniero_Insertar_Registro.php
@@ -6,26 +6,28 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
         <link rel="stylesheet" href="style.css">
         <div class="encabezado">
-            <img class="rol" src="Imagenes/rol_incentivos.png" alt=""><h1 class="titulo">Auxiliar Incentivos</h1><button type="submit" class="btn btn-primary">Salir</button>
+            <img class="rol" src="Imagenes/rol_ingeniero.jpg" alt=""><h1 class="titulo">Ingeniero Jefe De Planta</h1><button type="submit" class="btn btn-primary">Salir</button>
         </div>
-
-
     </head>
     <center><h1>Ingreso De Nuevo Registro</h1></center>
     <nav>
-        <ulclass="nav justify-content-center">
+        <ul class="nav justify-content-center">
             <ul class="nav nav-tabs">
                 <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="Rol_Incentivos_Ingresar.html">Ingresar</a>
+                    <a class="nav-link active" aria-current="page" href="Rol_Ingeniero_Ingresar.html">Ingresar</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link active" href="Rol_Incentivos_Consultar.html">Consultar</a>
+                    <a class="nav-link" href="Rol_Ingeniero_Actualizar.html">Actualizar</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="Rol_Ingeniero_Consultar.html">Consultar</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="Rol_Ingeniero_Eliminar.html">Eliminar</a>
                 </li>
             </ul>
-        </ul>
     </nav>
     <body>
-
         <?php
 
             //$busqueda = $_GET['buscar'];
@@ -75,6 +77,7 @@
             mysqli_close($conexion);
 
         ?>
+
 
 
 
